8d540f7c81353c4b5ea3e8246f23176da76a7eef
Gabrielle Brumer <gabriellebrumer@hotmail.com>
2022-07-28 10:49:59 -0300

    Criada a seeders de users

---
+++ src/database/seeders/20220728133214-seed_users.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    let users = [];
+    for(let i=1; i<=5; i++){
+      users.push({
+        id:  i,
+        nome: `usuario${i}`,
+        email: `usuario${i}@mail.com`,
+        senha: bcrypt.hashSync("123456", 10),
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      })
+    }
+    await queryInterface.bulkInsert('users', users, {}); 
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', null, {});
+  }
+};
